9ef5dcb417232fec5efc8ff835126aa21be4847c
DIV297 <91648671+DIV297@users.noreply.github.com>
2023-07-10 13:11:21 +0530

    dfsd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const connectToMongo = require("./db");
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
 app.use('/auth/user',require("./routes/user"));
 app.use('/auth/admin',require("./routes/admin"));
